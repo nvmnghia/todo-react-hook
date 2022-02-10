66380c9e083e164cd0d149f570a1fb8efd6c67ef
nvmnghia <nvmnghia@gmail.com>
2022-02-10 15:14:53 +0700

    feat: state `todos` in `App`

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
 
 import AddTodo from './add_todo/AddTodo';
+import Todo from './Todo';
 import TodoList from './todo_list/TodoList';
 
-function App() {
-  return (
-    <div className='container'>
-      <div className='row d-flex justify-content-center'>
-        <div className='col-md-8'>
-          <div className='row mt-4'>
-            <h2>TODO</h2>
+class App extends React.Component {
+  todos: Todo[] = [
+    new Todo('Something in the way she moves'),
+    new Todo(
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    ),
+  ];
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='row d-flex justify-content-center'>
+          <div className='col-md-8'>
+            <div className='row mt-4'>
+              <h2>TODO</h2>
+            </div>
+
+            <AddTodo />
+
+            <TodoList todos={this.todos} />
           </div>
-
-          <AddTodo />
-
-          <TodoList />
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/todo_list/TodoList.tsx
+++ src/todo_list/TodoList.tsx
@@ -1,19 +1,17 @@
 import Todo from '../Todo';
 import TodoItem from './todo_item/TodoItem';
 
-export default function TodoList() {
+interface TodoListProps {
+  todos: Todo[];
+}
+
+export default function TodoList({ todos }: TodoListProps) {
   return (
     <div className='row mt-4'>
       <div className='col'>
-        <TodoItem todo={new Todo('Something in the way she moves')} />
-
-        <TodoItem
-          todo={
-            new Todo(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-            )
-          }
-        />
+        {todos.map((todo) => (
+          <TodoItem todo={todo} />
+        ))}
       </div>
     </div>
   );
