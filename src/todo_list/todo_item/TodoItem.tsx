3641e2d4ae4af104b17893159bf1787bff423d49
nvmnghia <nvmnghia@gmail.com>
2022-02-10 17:11:25 +0700

    refactor: convert `TodoItem` to class component

--- src/todo_list/todo_item/TodoItem.tsx
+++ src/todo_list/todo_item/TodoItem.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faXmark } from '@fortawesome/free-solid-svg-icons';
+
 import Todo from '../../Todo';
 
 interface TodoItemProps {
@@ -8,24 +11,28 @@ interface TodoItemProps {
   remove: (id: number) => void;
 }
 
-export default function TodoItem(props: TodoItemProps) {
-  return (
-    <div className='d-flex align-items-start gap-2 p-2 mb-2 border border-1 rounded-3'>
-      {/* Content */}
-      <div className='flex-grow-1 align-self-center'>{props.todo.content}</div>
+export default class TodoItem extends React.Component<TodoItemProps, {}> {
+  render() {
+    return (
+      <div className='d-flex align-items-start gap-2 p-2 mb-2 border border-1 rounded-3'>
+        {/* Content */}
+        <div className='flex-grow-1 align-self-center'>
+          {this.props.todo.content}
+        </div>
 
-      {/* Edit */}
-      <button className='btn btn-outline-primary'>
-        <FontAwesomeIcon icon={faPen} />
-      </button>
+        {/* Edit */}
+        <button className='btn btn-outline-primary'>
+          <FontAwesomeIcon icon={faPen} />
+        </button>
 
-      {/* Remove */}
-      <button
-        className='btn btn-outline-danger'
-        onClick={() => props.remove(props.todo.id)}
-      >
-        <FontAwesomeIcon icon={faXmark} />
-      </button>
-    </div>
-  );
+        {/* Remove */}
+        <button
+          className='btn btn-outline-danger'
+          onClick={() => this.props.remove(this.props.todo.id)}
+        >
+          <FontAwesomeIcon icon={faXmark} />
+        </button>
+      </div>
+    );
+  }
 }
