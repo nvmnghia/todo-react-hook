cf72a09a90389cde7cfefd20a9d4cef5aab11669
nvmnghia <nvmnghia@gmail.com>
2022-02-10 10:10:08 +0700

    fix: align item buttons

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
       {/* Todo list */}
       <div className="row mt-4">
         <div className="col">
-          <div className='d-flex gap-2 p-2 mb-2 border border-1 rounded-3'>
+          <div className='d-flex align-items-start gap-2 p-2 mb-2 border border-1 rounded-3'>
             {/* Content */}
             <div className='flex-grow-1 align-self-center'>Something in the way she moves</div>
 
@@ -37,7 +37,7 @@ function App() {
             </button>
           </div>
 
-          <div className='d-flex gap-2 p-2 mb-2 border border-1 rounded-3'>
+          <div className='d-flex align-items-start gap-2 p-2 mb-2 border border-1 rounded-3'>
             <div className='flex-grow-1 align-self-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quo est optio necessitatibus! At nam animi vel hic perferendis vero culpa corporis autem dolorum iste. Fuga, quisquam at. Quae, ipsum!</div>
             <button className='btn btn-outline-primary'>
               <FontAwesomeIcon icon={faPen}/>
