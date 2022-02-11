1591c2825ac159bc6d46094df825300a61e1630b
nvmnghia <nvmnghia@gmail.com>
2022-02-11 16:01:28 +0700

    refactor: convert `<AddTodo>` to controlled form

--- src/add_todo/AddTodo.tsx
+++ src/add_todo/AddTodo.tsx
@@ -1,19 +1,42 @@
-import React, { FormEvent } from 'react';
+import React, { ChangeEvent, FormEvent } from 'react';
 
-export default class AddTodo extends React.Component {
+interface AddTodoState {
+  content: string;
+}
+
+export default class AddTodo extends React.Component<{}, AddTodoState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      content: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange({ target: { value } }: ChangeEvent<HTMLInputElement>) {
+    this.setState({ content: value });
+  }
+
+  handleSubmit(event: FormEvent) {
+    event.preventDefault();
+  }
+
   render() {
-    const preventSubmit = (event: FormEvent) => event.preventDefault();
-
     return (
       // TODO: Must a form be used here?
       <form
         className='row row-cols-md-auto g-3 align-items-center'
-        onSubmit={preventSubmit}
+        onSubmit={this.handleSubmit}
       >
         <div className='col-md-auto flex-grow-1'>
           <input
             type='text'
             className='form-control'
+            value={this.state.content}
+            onChange={this.handleChange}
             placeholder='Note something'
           />
         </div>
