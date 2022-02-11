ac174cd3b4fd188c90e6ed1d5c63ded5762aa7d5
nvmnghia <nvmnghia@gmail.com>
2022-02-11 14:51:50 +0700

    wip: convert `<TodoEditor>` to controlled form

--- src/todo_list/todo_item/TodoItemContent.tsx
+++ src/todo_list/todo_item/TodoItemContent.tsx
@@ -1,9 +1,39 @@
-export {};
+import React, { ChangeEvent } from 'react';
 
 const TodoContent = ({ content }: { content: string }) => <>{content}</>;
 
-const TodoEditor = ({ content }: { content: string }) => (
-  <textarea className='w-100' defaultValue={content}></textarea>
-);
+interface TodoEditorProps {
+  content: string;
+}
+
+interface TodoEditorState {
+  content: string;
+}
+
+class TodoEditor extends React.Component<TodoEditorProps, TodoEditorState> {
+  constructor(props: TodoEditorProps) {
+    super(props);
+
+    this.state = {
+      content: props.content,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value } }: ChangeEvent<HTMLTextAreaElement>) {
+    this.setState({ content: value });
+  }
+
+  render(): React.ReactNode {
+    return (
+      <textarea
+        className='w-100'
+        value={this.state.content}
+        onChange={this.handleChange}
+      ></textarea>
+    );
+  }
+}
 
 export { TodoContent, TodoEditor };
