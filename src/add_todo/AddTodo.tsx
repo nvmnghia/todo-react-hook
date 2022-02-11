359d2adc393af460b07b7a3a4661dbf5bf331e6e
nvmnghia <nvmnghia@gmail.com>
2022-02-11 15:49:12 +0700

    refactor: convert `<AddTodo>` to class component

--- src/add_todo/AddTodo.tsx
+++ src/add_todo/AddTodo.tsx
@@ -1,25 +1,27 @@
-import { FormEvent } from 'react';
+import React, { FormEvent } from 'react';
 
-export default function AddTodo() {
-  const preventSubmit = (event: FormEvent) => event.preventDefault();
+export default class AddTodo extends React.Component {
+  render() {
+    const preventSubmit = (event: FormEvent) => event.preventDefault();
 
-  return (
-    // TODO: Must a form be used here?
-    <form
-      className='row row-cols-md-auto g-3 align-items-center'
-      onSubmit={preventSubmit}
-    >
-      <div className='col-md-auto flex-grow-1'>
-        <input
-          type='text'
-          className='form-control'
-          placeholder='Note something'
-        />
-      </div>
+    return (
+      // TODO: Must a form be used here?
+      <form
+        className='row row-cols-md-auto g-3 align-items-center'
+        onSubmit={preventSubmit}
+      >
+        <div className='col-md-auto flex-grow-1'>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Note something'
+          />
+        </div>
 
-      <div className='col-md-auto col-xs-12'>
-        <button className='btn btn-primary w-100'>Add</button>
-      </div>
-    </form>
-  );
+        <div className='col-md-auto col-xs-12'>
+          <button className='btn btn-primary w-100'>Add</button>
+        </div>
+      </form>
+    );
+  }
 }
