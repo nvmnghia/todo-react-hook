cf9b1f9c5db01a8809a709985a26f6b27688ce56
nvmnghia <nvmnghia@gmail.com>
2022-02-14 05:56:16 +0700

    fix: do not actually save content before clicking Save
    Previously, the new content is saved/updated as the user types.
    Currently, this excessive saving is fixed.

--- src/todo_list/todo_item/TodoItem.tsx
+++ src/todo_list/todo_item/TodoItem.tsx
@@ -25,16 +25,20 @@ export default class TodoItem extends React.Component<
   TodoItemProps,
   TodoItemState
 > {
+  // Content waiting to be finally saved
+  tmpContent: string;
+
   constructor(props: TodoItemProps) {
     super(props);
 
     this.state = {
       editing: false,
     };
+    this.tmpContent = '';
 
     this.toggleEditingState = this.toggleEditingState.bind(this);
     this.remove = this.remove.bind(this);
-    this.save = this.save.bind(this);
+    this.updateTmpContent = this.updateTmpContent.bind(this);
   }
 
   toggleEditingState() {
@@ -49,15 +53,27 @@ export default class TodoItem extends React.Component<
     this.props.edit(this.props.todo.id, content);
   }
 
+  updateTmpContent(tmpContent: string) {
+    this.tmpContent = tmpContent;
+  }
+
   render() {
     const contentBox = this.state.editing ? (
-      <TodoEditor content={this.props.todo.content} save={this.save} />
+      <TodoEditor
+        content={this.props.todo.content}
+        save={this.updateTmpContent}
+      />
     ) : (
       <TodoContent content={this.props.todo.content} />
     );
 
     const editOrSaveButton = this.state.editing ? (
-      <SaveButton onSave={this.toggleEditingState} />
+      <SaveButton
+        onSave={() => {
+          this.toggleEditingState();
+          this.save(this.tmpContent);
+        }}
+      />
     ) : (
       <EditButton onStartEdit={this.toggleEditingState} />
     );
