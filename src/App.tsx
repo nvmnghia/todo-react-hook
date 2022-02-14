645dd38e6d86acf52c7b10fa315122c482f29339
nvmnghia <nvmnghia@gmail.com>
2022-02-14 14:26:21 +0700

    refactor: convert `<App>` to hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddTodo from './add_todo/AddTodo';
 import Todo from './Todo';
 import TodoList from './todo_list/TodoList';
 
-interface AppState {
-  todos: Todo[];
-}
+//================================================================================
+// Local Storage
+//================================================================================
 
-class App extends React.Component<unknown, AppState> {
-  constructor(props: unknown) {
-    super(props);
+const ITEM_KEY = 'todos';
 
-    this.state = {
-      todos: App.loadFromLocalStorage(),
-    };
-
-    this.addTodo = this.addTodo.bind(this);
-    this.editTodo = this.editTodo.bind(this);
-    this.removeTodo = this.removeTodo.bind(this);
+const loadFromLocalStorage = (): Todo[] => {
+  const json = localStorage.getItem(ITEM_KEY);
+  if (json === null) {
+    return [];
   }
 
-  addTodo(content: string) {
-    this.setState((state) => {
-      const todos = [...state.todos, new Todo(content)];
-      App.saveToLocalStorage(todos);
-      return { todos };
-    });
-  }
+  const tmpArr = JSON.parse(json);
+  return tmpArr.map((tmpTodo: any) => Todo.deserialize(tmpTodo));
+};
 
-  removeTodo(id: number) {
-    this.setState((state) => {
-      const todos = state.todos.filter((todo) => todo.id !== id);
-      App.saveToLocalStorage(todos);
-      return { todos };
-    });
-  }
+const saveToLocalStorage = (todos: Todo[]) => {
+  localStorage.setItem(ITEM_KEY, JSON.stringify(todos));
+};
 
-  editTodo(id: number, content: string) {
-    this.setState((state) => {
-      const todos = state.todos.map((todo) =>
-        todo.id === id ? todo.cloneNewContent(content) : todo
-      );
-      App.saveToLocalStorage(todos);
-      return { todos };
-    });
-  }
+//================================================================================
+// App
+//================================================================================
 
-  render() {
-    return (
-      <div className='container'>
-        <div className='row d-flex justify-content-center'>
-          <div className='col-md-8'>
-            <div className='row mt-4'>
-              <h2>TODO</h2>
-            </div>
+export default function App() {
+  const [todos, setTodos] = useState(loadFromLocalStorage());
 
-            <AddTodo add={this.addTodo} />
+  const addTodo = (content: string) => {
+    setTodos(
+      wrapTodosManipulator((prevTodos) => [...prevTodos, new Todo(content)])
+    );
+  };
+  const removeTodo = (id: number) => {
+    setTodos(
+      wrapTodosManipulator((prevTodos) =>
+        prevTodos.filter((todo) => todo.id !== id)
+      )
+    );
+  };
+  const editTodo = (id: number, content: string) => {
+    setTodos(
+      wrapTodosManipulator((prevTodos) =>
+        prevTodos.map((todo) =>
+          todo.id === id ? todo.cloneNewContent(content) : todo
+        )
+      )
+    );
+  };
 
-            <TodoList
-              todos={this.state.todos}
-              remove={this.removeTodo}
-              edit={this.editTodo}
-            />
+  return (
+    <div className='container'>
+      <div className='row d-flex justify-content-center'>
+        <div className='col-md-8'>
+          <div className='row mt-4'>
+            <h2>TODO</h2>
           </div>
+
+          <AddTodo add={addTodo} />
+
+          <TodoList todos={todos} remove={removeTodo} edit={editTodo} />
         </div>
       </div>
-    );
-  }
-
-  private static ITEM_KEY = 'todos';
-
-  private static saveToLocalStorage(todos: Todo[]) {
-    localStorage.setItem(this.ITEM_KEY, JSON.stringify(todos));
-  }
-
-  // TODO: rewrite this whole parsing method & the Todo class itself
-  private static loadFromLocalStorage(): Todo[] {
-    const json = localStorage.getItem(this.ITEM_KEY);
-    if (json === null) {
-      return [];
-    }
-
-    const tmpArr = JSON.parse(json);
-    return tmpArr.map((tmpTodo: any) => Todo.deserialize(tmpTodo));
-  }
+    </div>
+  );
 }
 
-export default App;
+type TodosManipulator = (prevTodos: Todo[]) => Todo[];
+// Add local storage save to function modifying todos
+// TODO: is this a suitable place?
+const wrapTodosManipulator = (
+  manipulator: TodosManipulator
+): TodosManipulator => {
+  return (prevTodos: Todo[]) => {
+    const todos = manipulator(prevTodos);
+    saveToLocalStorage(todos);
+    return todos;
+  };
+};
