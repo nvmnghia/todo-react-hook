757f151b34c4d2f4aa38e76d499e2dc6f379a473
nvmnghia <nvmnghia@gmail.com>
2022-02-14 13:05:05 +0700

    refactor: convert `<TodoItem>` to hook

--- src/todo_list/todo_item/TodoItem.tsx
+++ src/todo_list/todo_item/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Todo from '../../Todo';
 import {
@@ -22,81 +22,48 @@ interface TodoItemProps {
   edit: (id: number, content: string) => void;
 }
 
-interface TodoItemState {
-  editing: boolean;
-  tmpContent: string; // Content waiting to be finally saved
-}
+const TodoItem = (props: TodoItemProps) => {
+  const [editing, setEditing] = useState(false);
+  const toggleEditing = () => setEditing(!editing);
 
-export default class TodoItem extends React.Component<
-  TodoItemProps,
-  TodoItemState
-> {
-  constructor(props: TodoItemProps) {
-    super(props);
+  const [tmpContent, setTmpContent] = useState(props.todo.content);
 
-    this.state = {
-      editing: false,
-      tmpContent: props.todo.content,
-    };
+  const save = (content: string) => props.edit(props.todo.id, content);
+  const remove = () => props.remove(props.todo.id);
 
-    this.toggleEditingState = this.toggleEditingState.bind(this);
-    this.remove = this.remove.bind(this);
-    this.updateTmpContent = this.updateTmpContent.bind(this);
-  }
-
-  toggleEditingState() {
-    this.setState((state) => ({ editing: !state.editing }));
-  }
-
-  remove() {
-    this.props.remove(this.props.todo.id);
-  }
-
-  save(content: string) {
-    this.props.edit(this.props.todo.id, content);
-  }
-
-  updateTmpContent(tmpContent: string) {
-    this.setState({ tmpContent });
-  }
-
-  render() {
-    const contentBox = this.state.editing ? (
-      <TodoEditor
-        tmpContent={this.state.tmpContent}
-        onChange={this.updateTmpContent}
+  const contentBox = editing ? (
+    <TodoEditor tmpContent={tmpContent} onChange={setTmpContent} />
+  ) : (
+    <TodoContent content={props.todo.content} />
+  );
+  const editOrSaveButton = editing ? (
+    <>
+      <SaveButton
+        onSave={() => {
+          toggleEditing();
+          save(tmpContent);
+        }}
       />
-    ) : (
-      <TodoContent content={this.props.todo.content} />
-    );
+      <UndoButton onClick={toggleEditing} />
+    </>
+  ) : (
+    <EditButton onStartEdit={toggleEditing} />
+  );
 
-    const editOrSaveButton = this.state.editing ? (
-      <>
-        <SaveButton
-          onSave={() => {
-            this.toggleEditingState();
-            this.save(this.state.tmpContent);
-          }}
-        />
-        <UndoButton onClick={this.toggleEditingState} />
-      </>
-    ) : (
-      <EditButton onStartEdit={this.toggleEditingState} />
-    );
-
-    return (
-      <div className='p-2 mb-2 border border-1 rounded-3'>
-        <div className='d-flex align-items-start gap-2 mb-2'>
-          <div className='flex-grow-1 align-self-center text-muted'>
-            {this.props.todo.date.toLocaleString()}
-          </div>
-
-          {editOrSaveButton}
-
-          <RemoveButton onRemove={this.remove} />
+  return (
+    <div className='p-2 mb-2 border border-1 rounded-3'>
+      <div className='d-flex align-items-start gap-2 mb-2'>
+        <div className='flex-grow-1 align-self-center text-muted'>
+          {props.todo.date.toLocaleString()}
         </div>
-        {contentBox}
+
+        {editOrSaveButton}
+
+        <RemoveButton onRemove={remove} />
       </div>
-    );
-  }
-}
+      {contentBox}
+    </div>
+  );
+};
+
+export default TodoItem;
