57a160f9f9113dadafb32b45de186045409aeca0
nvmnghia <nvmnghia@gmail.com>
2022-02-23 09:36:11 +0700

    feat: add `title` for buttons

--- src/components/master_todo/todo_list/todo_item/TodoItemButtons.tsx
+++ src/components/master_todo/todo_list/todo_item/TodoItemButtons.tsx
@@ -9,25 +9,29 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 const EditButton = ({ onStartEdit }: { onStartEdit: () => void }) => (
-  <button className='btn btn-outline-primary' onClick={onStartEdit}>
+  <button
+    className='btn btn-outline-primary'
+    onClick={onStartEdit}
+    title='Edit'
+  >
     <FontAwesomeIcon icon={faPen} />
   </button>
 );
 
 const SaveButton = ({ onSave }: { onSave: () => void }) => (
-  <button className='btn btn-outline-primary' onClick={onSave}>
+  <button className='btn btn-outline-primary' onClick={onSave} title='Save'>
     <FontAwesomeIcon icon={faCheck} />
   </button>
 );
 
 const UndoButton = ({ onClick }: { onClick: () => void }) => (
-  <button className='btn btn-outline-danger' onClick={onClick}>
+  <button className='btn btn-outline-danger' onClick={onClick} title='Undo'>
     <FontAwesomeIcon icon={faRotateLeft} />
   </button>
 );
 
 const RemoveButton = ({ onRemove }: { onRemove: () => void }) => (
-  <button className='btn btn-outline-danger' onClick={onRemove}>
+  <button className='btn btn-outline-danger' onClick={onRemove} title='Remove'>
     <FontAwesomeIcon icon={faXmark} />
   </button>
 );
